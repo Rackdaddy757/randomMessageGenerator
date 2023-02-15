6672d18e569a11cd599c333dbf4de4979f58aee8
Lynnardo Holland <chefnardo@live.com>
2023-02-15 12:42:22 -0500

    Random message generator is functional

---
+++ randomMessages.js
@@ -0,0 +1,35 @@
+
+const city = ["Portsmouth", "Abilene", "Poughkeepsie", "New York"];
+const state = ["Virginia", "Texas", "California", "New York"];
+const rando = [
+    "The alpaca is know to hunt tigers with their bear hoves in this location!",
+    "The average pizza slice in this state is better than pizzas from Italy!",
+    "There was a Wooly Mammoth spotted here in 1976!",
+    "This city is the only city that Chuck Norris can't visit!"
+];
+
+/*Message generator formula
+-Message needs to take random index value from each array and place it into the message variable.
+-The function randomOutput gives me a random output for an array.
+-Each variable needs to have the function randomOutput applied onclick and return a new variable
+-How do I have the function store each new variable to place into the message variable
+${newCity}, ${newState}. Did you know that ${newRando}
+*/ 
+//Create formula that randomly selects an index from an array. 
+const messageGenerator = () => {
+const randomOutput = (arr) => {
+    let output = arr[Math.floor(Math.random()*arr.length)];
+    console.log(output)
+    return output;
+};
+let rCity = randomOutput(city);
+let rState = randomOutput(state);
+let rRando = randomOutput(rando);
+return `Here's a fun fact about ${rCity}, ${rState}. ${rRando}`;
+};
+
+const msgButton = document.getElementById("messageButton");
+msgButton.addEventListener("click", (event) => {
+    document.getElementById("rMessage").innerHTML= messageGenerator();
+})
+
